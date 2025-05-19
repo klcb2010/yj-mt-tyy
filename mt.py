d95fe0f2bde605c1504e3421be3e4bbcab82cf39
klcb2010 <32628414+klcb2010@users.noreply.github.com>
2025-05-20 07:12:30 +0800

    Update mt.py

--- mt.py
+++ mt.py
@@ -5,19 +5,20 @@
 cron:  5 0 * * *
 const $ = new Env("MT论坛");
 '''
-
-
 import requests
 import re
 import os
 import time
+import random
 
 # 设置ua
 ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
 session = requests.session()
 
+
 def myprint(msg):
     print(msg)
+
 
 def send_notification_message(title):
     try:
@@ -26,6 +27,7 @@ def send_notification_message(title):
     except Exception:
         pass
 
+
 try:
     if didibb == True:
         pass
@@ -33,6 +35,7 @@ try:
         pass
 except Exception:
     pass
+
 
 def pdwl():
     try:
@@ -43,18 +46,20 @@ def pdwl():
     except Exception:
         pass
 
+
 try:
     pdwl()
 except Exception:
     pass
+
 
 def main(username, password):
     headers = {'User-Agent': ua}
     session.get('https://bbs.binmt.cc', headers=headers)
     chusihua = session.get('https://bbs.binmt.cc/member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login', headers=headers)
     try:
-        loginhash = re.findall('loginhash=(.*?)">', chusihua.text)[0]
-        formhash = re.findall('formhash" value="(.*?)".*? />', chusihua.text)[0]
+        loginhash = re.findall('loginhash=(.*?)">', chusihua.text)
+        formhash = re.findall('formhash" value="(.*?)".*? />', chusihua.text)
     except Exception:
         return False
     denurl = f'https://bbs.binmt.cc/member.php?mod=logging&action=login&loginsubmit=yes&handlekey=login&loginhash={loginhash}&inajax=1'
@@ -67,30 +72,34 @@ def main(username, password):
         'questionid': '0',
         'answer': '',
     }
+    # 在登录操作前增加随机延迟
+    time.sleep(random.uniform(10, 20))
     denlu = session.post(headers=headers, url=denurl, data=data).text
-    
+
     if '欢迎您回来' in denlu:
-        fzmz = re.findall('欢迎您回来，(.*?)，现在', denlu)[0]
+        fzmz = re.findall('欢迎您回来，(.*?)，现在', denlu)
         # 去除用户级别（如“博士生”或“大学生”），假设级别和用户名以空格分隔
         username_only = fzmz.split(' ', 1)[-1] if ' ' in fzmz else fzmz
         myprint(f'{username_only}：登录成功')
         zbqd = session.get('https://bbs.binmt.cc/k_misign-sign.html', headers=headers).text
-        formhash = re.findall('formhash" value="(.*?)".*? />', zbqd)[0]
+        formhash = re.findall('formhash" value="(.*?)".*? />', zbqd)
         qdurl = f'https://bbs.binmt.cc/plugin.php?id=k_misign:sign&operation=qiandao&format=text&formhash={formhash}'
         qd = session.get(url=qdurl, headers=headers).text
-        qdyz = re.findall('<root><!\[CDATA\[(.*?)\]\]></root>', qd)[0]
+        qdyz = re.findall('<root><!$CDATA$$(.*?)$$$></root>', qd)
         myprint(f'签到状态：{qdyz}')
         if '已签' in qd:
             huoqu(formhash)
     return True
 
+
 def huoqu(formhash):
     headers = {'User-Agent': ua}
     huo = session.get('https://bbs.binmt.cc/k_misign-sign.html', headers=headers).text
-    jiang = re.findall('id="lxreward" value="(.*?)">', huo)[0]
+    jiang = re.findall('id="lxreward" value="(.*?)">', huo)
     myprint(f'签到奖励：{jiang}金币')
     tuic = f'https://bbs.binmt.cc/member.php?mod=logging&action=logout&formhash={formhash}'
     session.get(url=tuic, headers=headers)
+
 
 if __name__ == '__main__':
     username = ''
